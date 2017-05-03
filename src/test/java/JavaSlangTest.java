3e9e3d0056734a8423a8f8e94d14eb38ed30178e
Billy Fisher <billy.fisher@nike.com>
2017-05-02 18:03:03 -0700

    Added JavaSlangTest.basicFilter, gradle test successful.

--- src/test/java/JavaSlangTest.java
+++ src/test/java/JavaSlangTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import static javaslang.control.Either.right;
 import static javaslang.control.Option.none;
 import static javaslang.control.Option.some;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class JavaSlangTest {
@@ -36,5 +37,26 @@ public class JavaSlangTest {
     assertEquals(Option.of(null), none());
     assertNotEquals(some(null), some(none()));
     assertEquals(some(Option.of(null)), some(none()));
+  }
+
+  @Test
+  public void basicFilter() {
+    List<String> fruits = List.of("apple",
+        "pear",
+        "guava",
+        "watermelon",
+        "pineapple",
+        "kiwi",
+        "banana");
+
+    String beforeFilter = fruits.toString();
+    String afterFilter = fruits.filter(
+        incoming -> incoming.contains("pear"))
+        .toString();
+
+    assertThat(beforeFilter,
+        is("List(apple, pear, guava, watermelon, pineapple, kiwi, banana)"));
+    assertThat(afterFilter,
+        is("List(pear)"));
   }
 }
